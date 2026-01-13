bba29c6f114038e5e19a8a1abd54c5ea4bd5cbbf
jordonjones <jordon01jones@gmail.com>
2026-01-12 20:49:36 -0600

    not done but here

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+// **Is Pangram**
+// Write a function that checks if a string is a pangram
+// (contains every letter a–z).
+
 public class Main {
+    public static boolean isPan(String s){
+        for(int i = 0 ; i < s.length() ; i++){
+
+            if()
+        }
+    }
 }
